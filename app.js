c7795e9429394aa03eb522003c2e1a177c68e8ec
iH0DL <73050632+i-hodl@users.noreply.github.com>
2023-03-28 19:38:02 -0400

    .

--- app.js
+++ app.js
@@ -1,38 +1,64 @@
 const BACKGROUND_FOLDER = 'background';
 const FOREGROUND_FOLDER = 'foreground';
 const BASE_FOLDER = 'base';
+const EXTRACTIONS_FOLDER = 'extractions';
 
 let backgroundIndex = 1;
 let foregroundIndex = 1;
 let baseIndex = 1;
 
-function updateNFT() {
-  const currentDate = new Date();
-
+function combineNFT() {
   const background = document.getElementById('background');
   const foreground = document.getElementById('foreground');
+  const backgroundVideo = document.getElementById('background-video');
 
-  const backgroundSrc = `${BACKGROUND_FOLDER}/${backgroundIndex}.png`;
+  backgroundIndex = Math.floor(Math.random() * 9) + 1;
+  foregroundIndex = Math.floor(Math.random() * 6) + 1;
+
+  const backgroundSrc = `${BACKGROUND_FOLDER}/${backgroundIndex}`;
   const foregroundSrc = `${FOREGROUND_FOLDER}/${foregroundIndex}.png`;
 
-  const useBaseImage = Math.random() < 0.1;
-  if (useBaseImage) {
-    const baseSrc = `${BASE_FOLDER}/base_${baseIndex}.png`;
-    if (Math.random() < 0.5) {
-      background.src = baseSrc;
-    } else {
-      foreground.src = baseSrc;
-    }
+  checkFileExists(`${backgroundSrc}.mov`)
+    .then(() => {
+      backgroundVideo.src = `${backgroundSrc}.mov`;
+      backgroundVideo.style.display = 'block';
+      background.style.display = 'none';
+    })
+    .catch(() => {
+      background.src = `${backgroundSrc}.png`;
+      background.style.display = 'block';
+      backgroundVideo.style.display = 'none';
+    });
+
+  const useExtractionImage = Math.random() < 0.1;
+
+  if (useExtractionImage) {
+    const extractionIndex = Math.floor(Math.random() * 2) + 1;
+    const extractionSrc = `${EXTRACTIONS_FOLDER}/${extractionIndex}.png`;
+    foreground.src = extractionSrc;
+  } else {
+    foreground.src = foregroundSrc;
+    foregroundIndex = (foregroundIndex % 7) + 1;
   }
+}
 
-  background.src = backgroundSrc;
-  foreground.src = foregroundSrc;
-
-  foregroundIndex = (foregroundIndex % 3) + 1;
-  backgroundIndex = (backgroundIndex % 3) + 1;
-  baseIndex = (baseIndex % 18) + 1;
+function checkFileExists(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('HEAD', url, true);
+    xhr.onload = function () {
+      if (xhr.status === 200) {
+        resolve();
+      } else {
+        reject();
+      }
+    };
+    xhr.send();
+  });
 }
 
 const manualUpdateBtn = document.getElementById('manual-update-btn');
-manualUpdateBtn.addEventListener('click', updateNFT);
+manualUpdateBtn.addEventListener('click', combineNFT);
 
+const AUTO_UPDATE_INTERVAL = 500000; // 5000 milliseconds = 5 seconds
+setInterval(combineNFT, AUTO_UPDATE_INTERVAL);
---
+++ baseapp.js
@@ -0,0 +1,38 @@
+const BACKGROUND_FOLDER = 'background';
+const FOREGROUND_FOLDER = 'foreground';
+const BASE_FOLDER = 'base';
+
+let backgroundIndex = 1;
+let foregroundIndex = 1;
+let baseIndex = 1;
+
+function updateNFT() {
+  const currentDate = new Date();
+
+  const background = document.getElementById('background');
+  const foreground = document.getElementById('foreground');
+
+  const backgroundSrc = `${BACKGROUND_FOLDER}/${backgroundIndex}.png`;
+  const foregroundSrc = `${FOREGROUND_FOLDER}/${foregroundIndex}.png`;
+
+  const useBaseImage = Math.random() < 0.1;
+  if (useBaseImage) {
+    const baseSrc = `${BASE_FOLDER}/base_${baseIndex}.png`;
+    if (Math.random() < 0.5) {
+      background.src = baseSrc;
+    } else {
+      foreground.src = baseSrc;
+    }
+  }
+
+  background.src = backgroundSrc;
+  foreground.src = foregroundSrc;
+
+  foregroundIndex = (foregroundIndex % 10) + 1;
+  backgroundIndex = (backgroundIndex % 10) + 1;
+  baseIndex = (baseIndex % 18) + 1;
+}
+
+const manualUpdateBtn = document.getElementById('manual-update-btn');
+manualUpdateBtn.addEventListener('click', updateNFT);
+
